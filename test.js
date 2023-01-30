a6c973d8d793c2ba1b9973cc6df84b01ec57d22e
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2023-01-29 18:26:59 -0700

    initial commit

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
 // console.log('Hello Node!');
 // console.log('Goodbye.');
 
-const os = require('os');
-console.log('type : ' + os.type());
+// const os = require('os');
+// console.log('type : ' + os.type());
+
+// import { Buffer } from 'buffer';
+
+const bufs = Buffer.from([1, 2, 3, 4]);
+
+for (const buf of bufs) {
+console.log(buf);
+}
