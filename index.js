baae1e1cde1f23efafd1abd60ac0b095990e6e11
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2023-04-03 19:39:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -242,7 +242,8 @@ app.put('/users/:username',
 		let errors = validationResult(req);
 
 		if(!errors.isEmpty()) {
-			return res.status(400).json({ errors: errors.array() });
+			return res.status(422).json({ errors: errors.array() }),
+			console.log(req)
 		}
 	
 	let hashPassword = Users.hashPassword(req.body.password);
